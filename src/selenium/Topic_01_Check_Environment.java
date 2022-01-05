8859300571e04fe653ae92d6ab3a7ea7c040d7af
mira <daophuong2701@gmail.com>
2022-01-05 15:11:33 +0700

    second change

--- src/selenium/Topic_01_Check_Environment.java
+++ src/selenium/Topic_01_Check_Environment.java
@@ -24,6 +24,7 @@ public class Topic_01_Check_Environment {
 		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/");
+		//test
 	}
 
 	@Test
